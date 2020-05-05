3dccb48cfe42afa51b86e935a6a30dec4b8eb6c6
Steve Chan <megachan@gmail.com>
2020-05-04 18:43:36 -0700

    Increased NodeJS heap to 2048 due to out of memory error

--- elk-post-hooks.sh
+++ elk-post-hooks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # INSTALL ElastAlert plugin
-/opt/kibana/bin/kibana-plugin --allow-root install https://github.com/nsano-rururu/docker-sample/raw/master/elastalert-kibana-plugin/kibana/plugin/elastalert-kibana-plugin-1.1.0-7.5.2.zip
+if [ ! -d "/opt/kibana/plugins/elastalert-kibana-plugin" ]; then
+    /opt/kibana/bin/kibana-plugin --allow-root install https://github.com/nsano-rururu/docker-sample/raw/master/elastalert-kibana-plugin/kibana/plugin/elastalert-kibana-plugin-1.1.0-7.5.2.zip
+fi
 
 
