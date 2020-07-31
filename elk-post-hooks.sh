316dac8f2dadf33791a6bc4d9590adc83206770a
Steve Chan <megachan@gmail.com>
2020-07-31 09:21:47 -0700

    Changed ElastAlert plugin to 7.7.1

--- elk-post-hooks.sh
+++ elk-post-hooks.sh
@@ -2,7 +2,7 @@
 
 # INSTALL ElastAlert plugin
 if [ ! -d "/opt/kibana/plugins/elastalert-kibana-plugin" ]; then
-    /opt/kibana/bin/kibana-plugin --allow-root install https://github.com/nsano-rururu/docker-sample/raw/master/elastalert-kibana-plugin/kibana/plugin/elastalert-kibana-plugin-1.1.0-7.5.2.zip
+  /opt/kibana/bin/kibana-plugin install https://github.com/nsano-rururu/elastalert-kibana-plugin/releases/download/1.2.0/elastalert-kibana-plugin-1.2.0-7.7.1.zip --allow-root
 fi
 
 
